a98fd23fbfcd9990694a730966ffbdf3b34dc7d3
Jailani Abdul Rahman <jailani.har@googlemail.com>
2020-02-10 16:24:14 +0800

    Week06 slot01

---
+++ week06/slot01/RunProgram.java
@@ -0,0 +1,37 @@
+package week06.slot01;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class RunProgram extends Application {
+
+	public static Stage mainStage;
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		FXMLLoader fxmlLoader = 
+			new FXMLLoader(
+					getClass().getResource("./view/Login.fxml"));
+		VBox root = fxmlLoader.load();
+		
+//		FXMLLoader fxmlLoader =
+//			new FXMLLoader(
+//					getClass().getResource(
+//							"./view/Dashboard.fxml"));
+//		GridPane root = fxmlLoader.load();
+		
+		Scene scene = new Scene(root, 500, 500);
+		arg0.setScene(scene);
+		mainStage = arg0;
+		arg0.show();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
+}
---
+++ week06/slot01/controller/DashboardController.java
@@ -0,0 +1,14 @@
+package week06.slot01.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class DashboardController {
+
+	@FXML Label welcomeLabel;
+	
+	public void updateWelcomeLabel(String username) {
+		welcomeLabel.setText(welcomeLabel.getText() + ", " + username);
+	}
+	
+}
